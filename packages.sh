1d50dae15d5b542ca8a079afbb9753e9a5303f20
superp0s <crawfordt.014@gmail.com>
2014-11-28 00:19:14 -0500

    added projects folder for less clutter

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 mkdir debs
-dpkg-deb -b imgurloadingwheel debs
-dpkg-deb -b infiniloader debs
-dpkg-deb -b infiniboot4 debs
-dpkg-deb -b infiniboot5 debs
-dpkg-deb -b infiniboot6 debs
-dpkg-deb -b infiniboot6plus debs
+dpkg -b projects/imgurloadingwheel debs
+dpkg -b projects/infiniloader debs
+dpkg -b projects/infiniboot4 debs
+dpkg -b projects/infiniboot5 debs
+dpkg -b projects/infiniboot6 debs
+dpkg -b projects/infiniboot6plus debs
