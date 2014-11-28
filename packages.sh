dcff1109ca9d91c0d36a850c91eb668e13cb6ee2
superp0s <crawfordt.014@gmail.com>
2014-11-28 00:12:25 -0500

    added infinitboot packages

--- packages.sh
+++ packages.sh
@@ -1,5 +1,7 @@
 mkdir debs
 dpkg-deb -b imgurloadingwheel debs
 dpkg-deb -b infiniloader debs
-dpkg-deb -b infiniboot debs
+dpkg-deb -b infiniboot4 debs
+dpkg-deb -b infiniboot5 debs
 dpkg-deb -b infiniboot6 debs
+dpkg-deb -b infiniboot6plus debs
