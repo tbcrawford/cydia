d2f651849f0e36766a827150718868fd9d5f39b0
tcrawford14 <dev.tcrawford@gmail.com>
2015-06-25 15:13:59 -0400

    asld

--- packages.sh
+++ packages.sh
@@ -5,3 +5,5 @@ dpkg-deb -b projects/infiniboot5 debs
 dpkg-deb -b projects/infiniboot6 debs
 dpkg-deb -b projects/infiniboot6plus debs
 dpkg-deb -b projects/standardpackages debs
+dpkg-deb -b projects/loader debs
+
