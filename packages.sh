5663f12f65c8eda10e6e5d81980c3c3f56f34904
tcrawford14 <dev.tcrawford@gmail.com>
2015-06-25 15:25:42 -0400

    hopefulness

--- packages.sh
+++ packages.sh
@@ -1,9 +1,9 @@
-dpkg-deb -b projects/imgurloadingwheel debs
-dpkg-deb -b projects/infiniloader debs
-dpkg-deb -b projects/infiniboot4 debs
-dpkg-deb -b projects/infiniboot5 debs
-dpkg-deb -b projects/infiniboot6 debs
-dpkg-deb -b projects/infiniboot6plus debs
-dpkg-deb -b projects/standardpackages debs
-dpkg-deb -b projects/loader debs
+dpkg-deb -bZgzip projects/imgurloadingwheel debs
+dpkg-deb -bZgzip projects/infiniloader debs
+dpkg-deb -bZgzip projects/infiniboot4 debs
+dpkg-deb -bZgzip projects/infiniboot5 debs
+dpkg-deb -bZgzip projects/infiniboot6 debs
+dpkg-deb -bZgzip projects/infiniboot6plus debs
+dpkg-deb -bZgzip projects/standardpackages debs
+dpkg-deb -bZgzip projects/loader debs
 
