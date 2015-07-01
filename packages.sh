62d2f20268c5f87aee68e93983ba93e51ad315f6
tcrawford14 <dev.tcrawford@gmail.com>
2015-07-01 12:16:22 -0400

    updates

--- packages.sh
+++ packages.sh
@@ -1,9 +1,14 @@
-dpkg-deb -bZgzip projects/imgurloadingwheel debs
-dpkg-deb -bZgzip projects/infiniloader debs
-dpkg-deb -bZgzip projects/infiniboot4 debs
-dpkg-deb -bZgzip projects/infiniboot5 debs
-dpkg-deb -bZgzip projects/infiniboot6 debs
-dpkg-deb -bZgzip projects/infiniboot6plus debs
-dpkg-deb -bZgzip projects/standardpackages debs
-dpkg-deb -bZgzip projects/devrepos debs
+# dpkg-deb -bZgzip projects/imgurloadingwheel debs
+# dpkg-deb -bZgzip projects/infiniloader debs
+# dpkg-deb -bZgzip projects/infiniboot4 debs
+# dpkg-deb -bZgzip projects/infiniboot5 debs
+# dpkg-deb -bZgzip projects/infiniboot6 debs
+# dpkg-deb -bZgzip projects/infiniboot6plus debs
+# dpkg-deb -bZgzip projects/personalpackages debs
+# dpkg-deb -bZgzip projects/devrepos debs
 
+directories=`find projects -type d -maxdepth 1 | tail -n +2`
+
+for dir in $directories; do
+	dpkg-deb -bZgzip $dir debs
+done
