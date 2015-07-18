f24b773d1095d852556d368af4df474f2a346e44
tcrawford14 <dev.tcrawford@gmail.com>
2015-07-18 01:57:29 -0400

    basic clock minor changes

--- projects/ls-basic-clock/var/mobile/Library/GroovyLock/LS Basic Clock/config.js
+++ projects/ls-basic-clock/var/mobile/Library/GroovyLock/LS Basic Clock/config.js
@@ -2,4 +2,4 @@ var twentyfourhourtime = false;
 var showBackground = true;
 var timeColor = "#606060";
 var bgColor = "rgba(240, 240, 240, 0.5)";
-var clockPosFromTop = 500;
+var clockPosFromTop = 240;
