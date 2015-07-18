062a4488c485c129d5358d21c532eed442f9651a
tcrawford14 <dev.tcrawford@gmail.com>
2015-07-18 01:12:33 -0400

    basic clock minor improv

--- projects/ls-basic-clock/var/mobile/Library/GroovyLock/LS Basic Clock/config.js
+++ projects/ls-basic-clock/var/mobile/Library/GroovyLock/LS Basic Clock/config.js
@@ -4,12 +4,10 @@ var twentyfourhourtime = false;
 // Show background color = true; Don't show = false
 var showBackground = true;
 
-// The color of the clock, uncomment next line to revert to default color if forgotten
-// var timeColor = "#606060";
+// The color of the clock
 var timeColor = "#606060";
 
-// Background color behind the clock, uncomment to revert to default color if forgotten
-// var bgColor = "rgba(240, 240, 240, 0.5)";
+// Background color behind the clock
 var bgColor = "rgba(240, 240, 240, 0.5)";
 
 // The position in pixels that the clock will appear from the top of the screen
