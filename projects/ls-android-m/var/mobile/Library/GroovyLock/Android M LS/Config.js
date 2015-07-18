b4b4b2f69132667e31c9cc677937d8c76146c668
tcrawford14 <dev.tcrawford@gmail.com>
2015-07-18 13:36:41 -0400

    fixing config opts

--- projects/ls-android-m/var/mobile/Library/GroovyLock/Android M LS/Config.js
+++ projects/ls-android-m/var/mobile/Library/GroovyLock/Android M LS/Config.js
@@ -1,6 +1,6 @@
 var twentyFourHourTime = false;
-var timeColor = "#303030";
-var dateColor = "#303030";
+var timeColor = "#FFFFFF";
+var dateColor = "#FFFFFF";
 var clockPosFromTop = 100;
-var clockFont = 'Roboto-Regular'; // Options: 'Roboto-Regular', 'Roboto-Medium', 'Roboto-Thin', or 'Roboto-Light'
-var dateFont = 'Roboto-Regular';  // Options: 'Roboto-Regular', 'Roboto-Medium', 'Roboto-Thin', or 'Roboto-Light'
+var clockFont = "Roboto-Regular"; // Options: "Roboto-Regular", "Roboto-Medium", "Roboto-Thin", or "Roboto-Light"
+var dateFont = "Roboto-Regular";  // Options: "Roboto-Regular", "Roboto-Medium", "Roboto-Thin", or "Roboto-Light"
