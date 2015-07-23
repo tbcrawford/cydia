31c86250f1f7b01c5ac879aa4d5211c6dc67f35b
tcrawford14 <dev.tcrawford@gmail.com>
2015-07-22 21:17:35 -0400

    whoops-font now white

--- projects/ls-android-m/var/mobile/Library/GroovyLock/Android M LS/backup/Config.js
+++ projects/ls-android-m/var/mobile/Library/GroovyLock/Android M LS/backup/Config.js
@@ -1,6 +1,6 @@
 var twentyFourHourTime = false; // 24 hour clock - true; 12 hour clock - false
-var timeColor = "#000000"; // Color of the time
-var dateColor = "#000000"; // Color of the date
+var timeColor = "#FFFFFF"; // Color of the time
+var dateColor = "#FFFFFF"; // Color of the date
 var clockFontSize = "102px"; // The clock's font size
 var dateFontSize = "16px"; // The date's font size
 var clockPositionFromTop = 46; // Position from the top of the screen
