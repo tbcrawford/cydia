dfb3a24775f3d894b134428adaac146954b6d1fa
tcrawford14 <dev.tcrawford@gmail.com>
2015-07-29 21:19:11 -0400

    add lockhtml and sbhtml support

--- projects/ls-elemental/var/mobile/Library/GroovyLock/Elemental LS/backup/Config.js
+++ projects/ls-elemental/var/mobile/Library/GroovyLock/Elemental LS/backup/Config.js
@@ -1,8 +1,8 @@
 var twentyfourhourtime = false; // true - use 24 hour time ; false - use 12 hour time
 var showBackground = true; // true - show background ; false - don't show background
-var showShadow = true; // true - show drop shadow; false - don't show drop shadow
-var shadow = "6px 6px 6px 0px #888888"; // "horizontal-shadow-px | vertical-shadow-px | blur-dist-px | shadow-size-px | color-hex"
-var timeColor = "#606060"; // Hex, rgb, or rgba can be used
+var showShadow = false; // true - show drop shadow; false - don't show drop shadow
+var shadow = "6px 6px 6px 0px #202020"; // "horizontal-shadow-px | vertical-shadow-px | blur-dist-px | shadow-size-px | color-hex"
+var timeColor = "#404040"; // Hex, rgb, or rgba can be used
 var bgColor = "rgba(240, 240, 240, 0.5)"; // alpha controls opacity which holds a value between 0 and 1
 var clockPosFromTop = 240; // Unit: Pixels
 var boxWidth = 140; // Unit: Pixels
