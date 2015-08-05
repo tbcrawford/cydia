20bc73af22da7d7c4abdbc7462a72067e34a2d74
tcrawford14 <dev.tcrawford@gmail.com>
2015-08-05 14:40:55 -0400

    stop dilli-dallying

--- projects/ls-elemental/var/mobile/Library/GroovyLock/Elemental LS/scripts/clock.js
+++ projects/ls-elemental/var/mobile/Library/GroovyLock/Elemental LS/scripts/clock.js
@@ -60,9 +60,9 @@ function updateClock() {
 
     if (window.groovyAPI) {
         if (groovyAPI.isShowingNotifications()) {
-            $('html').animate({ 'top': "8%" }, 1000);
+            $('html').animate({ 'top': "8%" }, 800);
         } else {
-            $('html').animate({ 'top': clockPositionFromTop }, 250);
+            $('html').animate({ 'top': clockPositionFromTop }, 800);
         }
     }
 
